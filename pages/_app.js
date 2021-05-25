820eca3aa2316bdee62837588926bbaf87397752
Lucas Acosta <lucas2002lucas@gmail.com>
2021-05-25 11:02:57 +0200

    init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../styles/theme';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Title</title>
+      </Head>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
